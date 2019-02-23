658dd22ff2896daef874b420ba3f860e385ec386
Gastennui <42651340+Gastennui@users.noreply.github.com>
2019-02-23 13:17:00 -0800

    Update boxmuller.h
    Allows mean and standard deviation to be passed to gen samples and box mueller.

--- pa01/boxmuller.h
+++ pa01/boxmuller.h
@@ -3,4 +3,4 @@ using namespace std;
 
 double ranf(double );
 float box_muller(float , float);
-vector<vector<float> > genSamples();
+vector<vector<float> > genSamples(int meanx1, int meany1, int meanx2, int meany2, int stdx1, int stdy1, int stdx2, int stdy2);
